5e8e911b7c75f3fb6c78bb58409bf0b952eb124c
rayzorgod <rayzorgod@hotmail.com>
2020-06-23 12:07:50 +0100

    messing around with Validation

--- WizardV3/ClientApp/src/app/user-data-wizard/user-data-wizard.component.ts
+++ WizardV3/ClientApp/src/app/user-data-wizard/user-data-wizard.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Message } from 'primeng/api';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'user-data-wizard',
@@ -22,13 +23,32 @@ import { Message } from 'primeng/api';
         }
     `]
 })
-export class UserDataWizardComponent {
+export class UserDataWizardComponent implements OnInit {
   activeIndex: number = 0;
   firstName: string;
   lastName: string;
   address: string;
 
   msgs: Message[] = [];
+  form_SetupDDMandate_Step: FormGroup;
+
+  ngOnInit() {
+    this.form_SetupDDMandate_Step = new FormGroup({
+      accountHolderName: new FormControl('', Validators.required), 
+      sortCodeDigits0to2: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(2)]),
+      sortCodeDigits2to4: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(2)]),
+      sortCodeDigits4to6: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(2)]),
+      accountNumber: new FormControl()
+    });
+  }
+
+  form_SetupDDMandate_Step_Submit() {
+    console.log(this.form_SetupDDMandate_Step.value);
+    if (this.form_SetupDDMandate_Step.invalid) {
+      debugger;
+      return;
+    }
+  }
 
   next() {
     this.activeIndex++;
